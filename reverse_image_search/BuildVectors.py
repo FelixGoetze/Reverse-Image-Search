e651b90d8ecd421779bd4ce55f1fc8d323a7787d
Felix <afgoetze@gmail.com>
2021-01-04 14:37:24 +1100

    dictionary, pickle, progressbar

--- reverse_image_search/BuildVectors.py
+++ reverse_image_search/BuildVectors.py
@@ -2,31 +2,42 @@
 from img2vec_pytorch import Img2Vec
 from PIL import Image
 import glob
-import json
 import numpy as np
 import os
+import pickle
+from tqdm import tqdm
 
 input_path = "images/UnsplashAPIdataset/"
+img2vec = Img2Vec(cuda=False)
+
+# %%
+def compute_vector(image_key):
+    img = Image.open(input_path + image_key + ".jpg")
+    vector = img2vec.get_vec(img)
+    return vector
+
 
 # %% Rebuild vectors and imagelist and write to disk
-def rebuild_vectors(input_path):
-    img2vec = Img2Vec(cuda=False)
-    imagelist = [
-        os.path.basename(x).split(".")[0] for x in glob.glob(input_path + "*.jpg")
-    ]
-    vectors = np.ones((len(imagelist), 512))
-    for index, filename in enumerate(imagelist):
-        img = Image.open(input_path + filename + ".jpg")  # dry run
-        vector = img2vec.get_vec(img)  # dry run
-        # vector = np.ones((1, 512))  # dry run
-        vectors[index, :] = vector
-    with open("data/pictures.json", "w") as filehandle:
-        json.dump(imagelist, filehandle)
+def build_vectors(input_path="images/UnsplashAPIdataset/"):
+    # %%
+
+    # load all jpgs from as imagedict_jpgs
+    imagedict = {}
+    imagedict_jpgs = {
+        os.path.basename(x).split(".")[0]: None for x in glob.glob(input_path + "*.jpg")
+    }
+    vectors = np.ones((len(imagedict_jpgs), 512))
+    for index, key in enumerate(tqdm(imagedict_jpgs)):
+        # vectors[index, :] = compute_vector(key)
+        vectors[index, :] = np.ones((1, 512))
+        imagedict[key] = None
+    with open("data/images2.pickle", "wb") as filehandle:
+        pickle.dump(imagedict, filehandle, protocol=pickle.HIGHEST_PROTOCOL)
     vectors = vectors.astype("float32")
-    np.save("data/vectors.npy", vectors)
+    np.save("data/vectors2.npy", vectors)
+    # %%
     return
 
 
 # %%
-rebuild_vectors(input_path)
-# %%
+build_vectors(input_path)
