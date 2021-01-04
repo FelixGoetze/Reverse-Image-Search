e8e419aa3120cc345dad70c4e393d8bbdb90e36c
Felix <afgoetze@gmail.com>
2021-01-04 15:26:45 +1100

    save vectors when interrupted

--- reverse_image_search/BuildVectors.py
+++ reverse_image_search/BuildVectors.py
@@ -6,6 +6,7 @@ import numpy as np
 import os
 import pickle
 from tqdm import tqdm
+import time
 
 input_path = "images/UnsplashAPIdataset/"
 img2vec = Img2Vec(cuda=False)
@@ -17,24 +18,29 @@ def compute_vector(image_key):
     return vector
 
 
+def save_vectors():
+    with open("data/images2.pickle", "wb") as filehandle:
+        pickle.dump(imagedict, filehandle, protocol=pickle.HIGHEST_PROTOCOL)
+    vectors = vectors.astype("float32")
+    np.save("data/vectors2.npy", vectors)
+
+
 # %% Rebuild vectors and imagelist and write to disk
 def build_vectors(input_path="images/UnsplashAPIdataset/"):
     # %%
-
     # load all jpgs from as imagedict_jpgs
     imagedict = {}
     imagedict_jpgs = {
         os.path.basename(x).split(".")[0]: None for x in glob.glob(input_path + "*.jpg")
     }
     vectors = np.ones((len(imagedict_jpgs), 512))
-    for index, key in enumerate(tqdm(imagedict_jpgs)):
-        # vectors[index, :] = compute_vector(key)
-        vectors[index, :] = np.ones((1, 512))
-        imagedict[key] = None
-    with open("data/images2.pickle", "wb") as filehandle:
-        pickle.dump(imagedict, filehandle, protocol=pickle.HIGHEST_PROTOCOL)
-    vectors = vectors.astype("float32")
-    np.save("data/vectors2.npy", vectors)
+    try:
+        for index, key in enumerate(tqdm(imagedict_jpgs)):
+            vectors[index, :] = compute_vector(key)
+            imagedict[key] = None
+    except KeyboardInterrupt:
+        save_vectors
+    save_vectors
     # %%
     return
 
